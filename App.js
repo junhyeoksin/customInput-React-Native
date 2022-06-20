45fa4270b986c19a46bba5824bcddd6db44bb35f
Jun Hyeok Shin <h-kun@naver.com>
2022-06-21 08:58:01 +0900

    Update:

--- App.js
+++ App.js
@@ -2,10 +2,13 @@
  import { View } from "react-native";
  import InputTest from "./screen/InputTest";
  import UseReducerInputTest from "./screen/UseReducerInputTest";
+ import KeyboardAvoidingViewTest from "./screen/KeyboardAvoidingViewTest";
 
  const App = () => {
    return (
-        <UseReducerInputTest/>
+        // <UseReducerInputTest/>
+        <KeyboardAvoidingViewTest/>
+
    );
  };
 
