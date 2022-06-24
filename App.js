b58d734104271bcf627bab2ae66eb2d2ba8e4284
Jun Hyeok Shin <h-kun@naver.com>
2022-06-24 15:55:27 +0900

    Update:

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
- import React from "react";
- import { View } from "react-native";
- import InputTest from "./screen/InputTest";
- import UseReducerInputTest from "./screen/UseReducerInputTest";
- import KeyboardAvoidingViewTest from "./screen/KeyboardAvoidingViewTest";
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import RegistrationScreen from './src/views/screens/RegistrationScreen';
+import LoginScreen from './src/views/screens/LoginScreen';
+import HomeScreen from './src/views/screens/HomeScreen';
 
- const App = () => {
-   return (
-        // <UseReducerInputTest/>
-        <KeyboardAvoidingViewTest/>
-
-   );
- };
-
- export default App;
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOption={{headerShown: false}}>
+        <Stack.Screen
+          name="RegistrationScreen"
+          component={RegistrationScreen}
+        />
+        <Stack.Screen name={'LoginScreen'} component={LoginScreen} />
+        <Stack.Screen name={'HomeScreen'} component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App;
+//  <UseReducerInputTest />
+// <KeyboardAvoidingViewTest/>
 
